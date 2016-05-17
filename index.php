aa1d91459f3673b7068e898f4e9da96884fa36d7
stan wielga <svwielga4@gmail.com>
2016-05-17 11:04:35 -0700

    layout out html skele

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+    <html>
+        <head>
+            <title>StanChat</title>
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+            <!-- Latest compiled JavaScript -->
+            <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" href="./assets/style.css" type="text/css"/>
+            <script src="./assets/script.js"></script>
+        </head>
+        <body>
+            <h1>Welcome to StanChat!</h1>
+            <h2>Please Login</h2>
+            <form>
+                <input type="text" placeholder="Username">
+                <input type="password" placeholder="Password">
+                <button>Submit</button>
+                <button>Register</button>
+            </form>
+
+        </body>
+    </html>
