3feeb2acd61e533085a50253e9794240604f13d9
stan wielga <svwielga4@gmail.com>
2016-05-21 08:37:01 -0700

    fixed #2, you can use apostrophes now

--- assets/chat_post.php
+++ assets/chat_post.php
@@ -2,7 +2,7 @@
 session_start();
 require('mysql_connect.php');
 $username = $_SESSION['users_id'];
-$text = $_POST['text'];
+$text = addslashes($_POST['text']);
 $query = "INSERT INTO `chat_history`(`user`, `text`) VALUES ('$username','$text')";
 $result = mysqli_query($conn, $query);
 $output = ['success'=>false];
@@ -10,8 +10,7 @@ if(mysqli_num_rows($result)) {
     $output = ['success'=>true];
     while ($row = mysqli_fetch_assoc($result)) {
         $output['data'][] = $row;
-//        $output = ['success' => true, 'user_id' => $output['data'][0]['id']];
-//        $_SESSION['users_id']= $output['user_id'];
+
     }
 }
 print(json_encode($output));
