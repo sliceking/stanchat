451e4d0bd2dfa28607253e931136be1d846256c0
stan wielga <svwielga4@gmail.com>
2016-05-21 11:42:58 -0700

    added local libraries

--- index.php
+++ index.php
@@ -11,12 +11,9 @@
     <html>
         <head>
             <title>StanChat</title>
-            <!-- Latest compiled and minified CSS -->
-            <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-            <!-- jQuery library -->
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-            <!-- Latest compiled JavaScript -->
-            <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" href="./assets/bootstrap.min.css">
+            <script src="./assets/jquery.min.js"></script>
+            <script src="./assets/bootstrap.min.js"></script>
             <script src="./assets/linkify.min.js"></script>
             <script src="./assets/linkify-jquery.min.js"></script>
             <link rel="stylesheet" href="./assets/style.css" type="text/css"/>
