51e49ea883c2164accb1836e0d08cf00a405c8ae
stan wielga <svwielga4@gmail.com>
2016-06-05 23:23:24 -0700

    refined operations

--- assets/operations.php
+++ assets/operations.php
@@ -1,8 +1,36 @@
 <?php
 
+include('connection.php');
+
+$data = ['success' => false];
+
 $operation = $_POST['operation'];
 
-if (operation == 'login')
+if ($operation == 'login'){
+    include('login.php');
+
+}elseif ($operation == 'add_user'){
+    include('add_user.php');
+
+}elseif ($operation == 'chat_post'){
+    include ('chat_post.php');
+
+}elseif ($operation == 'fetch_users'){
+    include('fetch_users.php');
+
+}elseif ($operation == 'fetch_history'){
+    include('fetch_history.php');
+
+}elseif ($operation == 'logout'){
+    include('logout.php');
+
+}else {
+    $data['errors'] = 'invalid operation';
+
+}
+
+$data = json_encode($data);
+print_r($data);
 
 
 ?>
